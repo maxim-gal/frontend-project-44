9ce16db13bfded6577df713dac31b304edfea608
Maxim Galimnassov <galimmaxim@gmail.com>
2025-05-11 23:29:55 +0300

    added eslint, stylistic, badges

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default defineConfig([
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      
+      'no-console': 'off',
+      'import/extensions': 'off',
+    },
+  },
+  { files: ['/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+]);
